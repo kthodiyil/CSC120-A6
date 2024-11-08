9135a00b0880ff1724ce1e9db5cb9239f99bff2c
Kavitha Thodiyil <kthodiyil@smith.edu>
2024-11-07 19:26:01 -0500

    sync

--- House.java
+++ House.java
@@ -1,10 +1,42 @@
 /* This is a stub for the House class */
-public class House {
-
-  public House() {
+import java.util.ArrayList;
+public class House extends Building {
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+  
+  public House(String name, String address, int nFloors, ArrayList<String> residents, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = residents;
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
   }
 
+  public boolean hasDiningRoom() {
+    if(this.hasDiningRoom == true){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public int nResidents(){
+    return this.residents.size();
+  }
+
+  public void moveIn(String name){
+    this.residents.add(name);
+  }
+
+  public String moveOut(String name){
+    int numRes = this.residents.size() - 1;
+    this.residents.remove(numRes);
+    return name;
+  }
+  public boolean isInArray(String name){
+    return this.residents.contains(name);
+  }
+  
   public static void main(String[] args) {
     new House();
   }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,7 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+    I really like using inheritance in coding because it makes it a lot easier to code between classes.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    I would say that referring to the class coding examples to understand the specific inheritance syntax is really useful. Also, the slideshows are always a great resource so refer back to those too.
+    Additionally, I love the oracle doc and it is so useful for finding built in methods to use.
